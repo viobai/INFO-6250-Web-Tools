1a10207a61c1fd57869ea64b3590ab33aa5a4a11
Viola Bai <bai.tian@northeastern.edu>
2021-10-06 08:52:22 -0700

    Adds server logic and object classes

---
+++ work/express-login/session.js
@@ -0,0 +1,22 @@
+let sessions = {};
+
+function getUsernameBySessionId({ sid }) {
+  return sessions[sid];
+}
+
+function addSession({ sid, username }) {
+  sessions[sid] = username;
+}
+
+function removeSession({ sid }) {
+  delete sessions[sid];
+}
+
+const session = {
+  sessions,
+  getUsernameBySessionId,
+  addSession,
+  removeSession
+};
+
+module.exports = session;
